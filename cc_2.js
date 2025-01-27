3b42f477228ecceba18eb6a63102888d5c503192
Madisyn Notz <madisynnotz33@gmail.com>
2025-01-26 20:39:47 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -8,4 +8,12 @@ console.log(products);
 products.pop();    
 console.log("Updated list of products:",products);    
 
-   
+ // Task 2: Accessing and Modifying Arrays //
+let scores = [80, 83, 85, 90, 95];    
+scores[1] = 83;  
+let total = scores.reduce((a, b) => a + b, 0);  
+let averageScore = total / scores.length;    
+
+
+console.log("Updated list of Scores:", scores);    
+console.log("Average of Scores:",83);  
