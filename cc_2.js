5467a230cd8c16240112c494bf8c15e9cf7eda85
Madisyn Notz <madisynnotz33@gmail.com>
2025-01-26 20:02:46 -0500

    Task 1 - Store Inventory

--- cc_2.js
+++ cc_2.js
@@ -11,12 +11,12 @@ console.log("Updated list of products:",products);
 
 // Task 2: Accessing and Modifying Arrays //
 let scores = [80, 83, 85, 90, 95];    
-scores[1] = 95;   
+scores[1] = 83;   
 let total = scores.reduce((a, b) => a + b, 0);   
 let averageScore = total / scores.length;    
 
 console.log("Updated list of Scores:", scores);    
-console.log("Average of Scores:",average);   
+console.log("Average of Scores:",83); 
 
 // Task 3: Working with Objects //
 let employee = {
@@ -35,24 +35,24 @@ console.log("updated employee record");
 
 // Task 4: Array of Objects //
 let customers = [
-    {name: "Joe Smith", email: "smith.joe@gmail.com", purchaseAmount: 100},
-    {name: "Emily Kingston", email: "kingston.emily@gmail.com", purchaseAmount: 200},
-    {name: "Kendall Vertes", email: "vertes.kendall@gmail.com", purchaseAmount: 300}
+    {name: "Joe Smith", email: "smith.joe@gmail.com", purchaseAmount: 200},
+    {name: "Emily Kingston", email: "kingston.emily@gmail.com", purchaseAmount: 400},
+    {name: "Kendall Vertes", email: "vertes.kendall@gmail.com", purchaseAmount: 100}
 ];   
 
 console.log(customers);    
 
-customers.push({name: "Franklin Clinton", email: "Clinton@gmail.com", purchaseAmount: 400});    
+customers.push({name: "Sarah Vincent", email: "vincent.sarah@gmail.com", purchaseAmount: 300});    
 
 console.log(customers);    
 
 // Task 5: Object Methods //
 let order = {
-    orderId: "101",
-    customerName: "Tyrone Biggums",
-    amount: 50,
+    orderId: "100",
+    customerName: "Kendall Vertes",
+    amount: 75,
     calculateTax: function() {
-        const taxRate = 0.10;   
+        const taxRate = 0.07;   
         return this.amount * taxRate;    
     }
 };    
